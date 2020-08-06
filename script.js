34bbb3377c4fbb862676ce0c673e68eac260c7e8
Ryan Talbot <ryan-talbot@outlook.com>
2020-08-06 23:26:33 +0100

    Bug fix

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 const urlBase = 'https://api.punkapi.com/v2/beers';
 
 async function getBeers() {
+  // fetch
   const beerPromise = await fetch(urlBase);
   const beers = await beerPromise.json();
+
+  // render data
   const beersDiv = document.querySelector('.beers');
 
-  let beerHTML = '';
+  let beerHtml = '';
+
+  // Add the rest of the beer data to the HTML template.
 
   beers.forEach((beer) => {
     beerHtml += `
-     <div class='beer-wrapper card'>
-         <div class='beer'>
-             <img class='beer__img' src="${beer.image_url}">
-             <h3>${beer.name}</h3>
-             <span class='beer__info'>
-                 <span>ABV: ${beer.abv}%</span>
-                 <span>IBU: ${beer.ibu}</span>
-             </span>
-         </div>
-     </div>
-    `;
+        <div class='beer-wrapper card'>
+            <div class='beer'>
+                <img class='beer__img' src="${beer.image_url}">
+                <h3>${beer.name}</h3>
+                <span class='beer__info'>
+                    <span>ABV: ${beer.abv}%</span>
+                    <span>IBU: ${beer.ibu}</span>
+                </span>
+            </div>
+        </div>
+       `;
   });
 
-  beersDiv.innerHTML = beerHTML;
+  beersDiv.innerHTML = beerHtml;
 }
 
 getBeers();
