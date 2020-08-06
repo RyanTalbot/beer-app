17c52ccff4db7a5f6d1a90c16e0fc0c5ad11fb50
Ryan Talbot <ryan-talbot@outlook.com>
2020-08-06 23:19:30 +0100

    getBeers function

---
+++ script.js
@@ -0,0 +1,28 @@
+const urlBase = 'https://api.punkapi.com/v2/beers';
+
+async function getBeers() {
+  const beerPromise = await fetch(urlBase);
+  const beers = await beerPromise.json();
+  const beersDiv = document.querySelector('.beers');
+
+  let beerHTML = '';
+
+  beers.forEach((beer) => {
+    beerHtml += `
+     <div class='beer-wrapper card'>
+         <div class='beer'>
+             <img class='beer__img' src="${beer.image_url}">
+             <h3>${beer.name}</h3>
+             <span class='beer__info'>
+                 <span>ABV: ${beer.abv}%</span>
+                 <span>IBU: ${beer.ibu}</span>
+             </span>
+         </div>
+     </div>
+    `;
+  });
+
+  beersDiv.innerHTML = beerHTML;
+}
+
+getBeers();
