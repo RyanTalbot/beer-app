aee7753ae6098869b95709cfe3152850faaee26b
Ryan Talbot <ryan-talbot@outlook.com>
2020-08-07 22:43:51 +0100

    Pagination

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const urlBase = 'https://api.punkapi.com/v2/beers';
+// variables
+const urlBase = 'https://api.punkapi.com/v2/beers?page=';
 const filterABV = document.getElementById('filterABV');
 const filterIBU = document.getElementById('filterIBU');
+const pageText = document.getElementById('pageNumber');
+const prevPage = document.getElementById('prevPage');
+const nextPage = document.getElementById('nextPage');
 let optionsABV = '',
-  optionsIBU = '';
+  optionsIBU = '',
+  page = 1;
 
 // filters
 filterABV.addEventListener('change', (e) => {
@@ -13,16 +18,17 @@ filterABV.addEventListener('change', (e) => {
       optionsABV = '';
       break;
     case 'weak':
-      optionsABV = 'abv_lt=4.6';
+      optionsABV = '&abv_lt=4.6';
       break;
     case 'medium':
-      optionsABV = 'abv_gt=4.5&abv_lt=7.6';
+      optionsABV = '&abv_gt=4.5&abv_lt=7.6';
       break;
     case 'strong':
-      optionsABV = 'abv_gt=7.5';
+      optionsABV = '&abv_gt=7.5';
       break;
   }
 
+  page = 1;
   getBeers();
 });
 
@@ -34,31 +40,44 @@ filterIBU.addEventListener('change', (e) => {
       optionsIBU = '';
       break;
     case 'weak':
-      optionsIBU = 'ibu_lt=35';
+      optionsIBU = '&ibu_lt=35';
       break;
     case 'medium':
-      optionsIBU = 'ibu_gt=34&ibu_lt=75';
+      optionsIBU = '&ibu_gt=34&ibu_lt=75';
       break;
     case 'strong':
-      optionsIBU = 'ibu_gt=74';
+      optionsIBU = '&ibu_gt=74';
       break;
   }
 
+  page = 1;
   getBeers();
 });
 
 async function getBeers() {
-  const url = urlBase + '?' + optionsABV + '&' + optionsIBU;
+  const url = urlBase + page + optionsABV + optionsIBU;
   // fetch
   const beerPromise = await fetch(url);
   const beers = await beerPromise.json();
+
+  // pagination
+  pageText.innerText = page;
+
+  if (page === 1) {
+    prevPage.disabled = true;
+  } else {
+    prevPage.disabled = false;
+  }
+  if (beers.length < 25) {
+    nextPage.disabled = true;
+  } else {
+    nextPage.disabled = false;
+  }
 
   // render data
   const beersDiv = document.querySelector('.beers');
 
   let beerHtml = '';
-
-  // Fill in the blanks with the rest of the data
 
   beers.forEach((beer) => {
     beerHtml += `
@@ -85,6 +104,16 @@ async function getBeers() {
 
   beersDiv.innerHTML = beerHtml;
 }
+
+// pagination
+prevPage.addEventListener('click', () => {
+  page--;
+  getBeers();
+});
+nextPage.addEventListener('click', () => {
+  page++;
+  getBeers();
+});
 
 // initial get
 getBeers();
